e37aeaedfee30e63deea77969ddb25d7c7e4c667
KotelnikovKP <kotelnikov1302@gmail.com>
2023-05-07 20:45:48 +0500

    Saving kline data to db
    Add KlineHistory model. Replace get_public_trade_history() method with get_kline() method in scheduler handler to get more data from Bybit. Create two classes IterationStack and Iteration for handling data from the exchange (in the current commit only saving to database). Add a task to the schedule to clear memory from old data in IterationStack.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
-from sqlalchemy import Column, String, Numeric, Boolean
+from datetime import datetime
+from decimal import Decimal
+from typing import List
+
+from sqlalchemy import String, Numeric, Boolean, DateTime, ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 from app.config import DATABASE_HOST, DATABASE_NAME, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD
 
@@ -25,20 +29,44 @@ def get_async_session():
 
 
 class Symbol(Base):
-
     __tablename__ = 'symbol'
 
-    symbol = Column(String(15), primary_key=True)
-    min_leverage = Column(Numeric(19, 9), default=1.0, nullable=False)
-    max_leverage = Column(Numeric(19, 9), default=25.0, nullable=False)
-    leverage_step = Column(Numeric(19, 9), default=0.01, nullable=False)
-    min_price = Column(Numeric(19, 9), default=0.01, nullable=False)
-    max_price = Column(Numeric(19, 9), default=10000.0, nullable=False)
-    tick_size = Column(Numeric(19, 9), default=0.01, nullable=False)
-    min_order_qty = Column(Numeric(19, 9), default=0.1, nullable=False)
-    max_order_qty = Column(Numeric(19, 9), default=1000000.0, nullable=False)
-    qty_step = Column(Numeric(19, 9), default=0.1, nullable=False)
-    is_active = Column(Boolean(), default=True)
+    symbol: Mapped[str] = mapped_column(String(15), primary_key=True, index=True)
+    min_leverage: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=1.0, nullable=False)
+    max_leverage: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=25.0, nullable=False)
+    leverage_step: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.01, nullable=False)
+    min_price: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.01, nullable=False)
+    max_price: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=10000.0, nullable=False)
+    tick_size: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.01, nullable=False)
+    min_order_qty: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.1, nullable=False)
+    max_order_qty: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=1000000.0, nullable=False)
+    qty_step: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.1, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), default=True)
+    kline_history: Mapped[List['KlineHistory']] = relationship(back_populates='symbol', cascade='all, delete-orphan')
 
     def __str__(self):
         return f'{self.symbol} ({self.is_active})'
+
+
+class KlineHistory(Base):
+    __tablename__ = 'kline_history'
+
+    time_kline: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    symbol_key: Mapped[str] = mapped_column(ForeignKey('symbol.symbol'), nullable=False)
+    symbol: Mapped['Symbol'] = relationship(back_populates='kline_history')
+    open_price: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.0, nullable=False)
+    high_price: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.0, nullable=False)
+    low_price: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.0, nullable=False)
+    close_price: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.0, nullable=False)
+    volume: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.0, nullable=False)
+    turnover: Mapped[Decimal] = mapped_column(Numeric(19, 9), default=0.0, nullable=False)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('symbol_key', 'time_kline', name='key_time'),
+    )
+
+    def __str__(self):
+        return f'{self.time_kline} - {self.symbol_key}: (' \
+               f'O={self.open_price}), H={self.high_price}), ' \
+               f'L={self.low_price}), C={self.close_price}), ' \
+               f'V={self.volume}), T={self.turnover})'
